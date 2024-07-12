0c74fff74bb056bd9717be4e4957e177271420a3
lukamrko <lukamrko@gmail.com>
2024-07-12 17:01:17 +0200

    End of lection

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,21 +1,32 @@
-function List() {
-    const fruits = [
-        { id: 1, name: "apple", calories: 95 },
-        { id: 2, name: "orange", calories: 45 },
-        { id: 3, name: "banana", calories: 105 },
-        { id: 4, name: "coconut", calories: 159 },
-        { id: 5, name: "pineapple", calories: 37 },
-    ];
+import PropTypes from 'prop-types'
 
-    // fruits.sort((a, b) => a.name.localeCompare(b.name));
-    fruits.sort((a, b) => a.calories - b.calories);
+function List(props) {
+    const category = props.category;
+    const itemsList = props.items;
 
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
-
-    const listItems = highCalFruits.map(fruit =>
+    const listItems = itemsList.map(fruit =>
         <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b> </li>);
-    return (<ol>{listItems}</ol>);
+    
+    return (
+        <>
+            <h3 className="list-category">{category}</h3>
+            <ol className="list-items">{listItems}</ol>
+        </>);
+}
+
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            name: PropTypes.string,
+            calories: PropTypes.number
+        })),
+}
+
+List.defaultProps = {
+    category: "Category",
+    items: []
 }
 
 export default List
